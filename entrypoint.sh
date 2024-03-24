7c4d3d83878c3129c888e7f57e0c66becc6772a2
__plb__ <plbayart@gmail.com>
2024-03-24 19:58:44 +0100

    update config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
 # Migrations de la base de données
+echo "Création BDD"
 python manage.py makemigrations
 python manage.py migrate --noinput
+
+echo "Collecte des fichiers statiques"
+python manage.py collectstatic --noinput
 
 # Remplir la base de données
 python manage.py init_vigicrue
